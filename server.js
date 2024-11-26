8389d73226a6ce07fe24f07cba61e5b52bcdfd46
VictoZache <victorzache27@gmail.com>
2024-11-25 23:05:33 -0300

    Aula 2

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        desc: "Uma foto",
+        image: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        desc: "Uma foto",
+        image: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        desc: "Uma foto",
+        image: "https://placecats.com/millie/300/150"
+    }
+];
+
 const app = express();
+
+app.use (express.json());
+
 app.listen(3000, () => {
     console.log("Servido esta ouvindo.");
     
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Bem Vindo!");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
 
-console.log("Teste de alteração no arquivo");
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
+
+});
+
+
